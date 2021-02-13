e4026079eaf2d4b7d327b283f3dc50041e37e71b
htest <htest@gmail.com>
2021-02-13 19:44:02 +0530

    commit1

---
+++ Automation/src/testclasses/TestNG_Priority.java
@@ -0,0 +1,31 @@
+package testclasses;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestNG_Priority {
+    @BeforeClass
+    public void setUp() {
+        System.out.println("Test Priority > before Class");
+    }
+
+    @AfterClass
+    public void cleanUp() {
+        System.out.println("Test Priority > after Class");
+    }
+    @Test
+    public void testMethod1() {
+        System.out.println("Test Priority > tsetMethod1");
+    }
+
+    @Test
+    public void testMethod2() {
+        System.out.println("Test Priority > tsetMethod2");
+    }
+    @Test
+    public void testMethod3() {
+        System.out.println("Test Priority > tsetMethod3");
+    }
+
+}
