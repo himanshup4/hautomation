517736b44b1223af0feda4498ed6ae7ea5689729
herain <herainp@gmail.com>
2021-02-14 15:34:38 +0800

    adding jar to directory

--- Automation/src/amazonhelper/BroswerInit.java
+++ Automation/src/amazonhelper/BroswerInit.java
@@ -9,18 +9,19 @@ import java.util.concurrent.TimeUnit;
 
 public class BroswerInit {
     WebDriver driver;
+    String currentDir = System.getProperty("user.dir");
 
     public WebDriver initializeDriver(String browser){
         if(browser.equals("chrome")) {
-            System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\driver\\chromedriver.exe"); // setup system environment variable for chrome driver
+            System.setProperty("webdriver.chrome.driver", currentDir + "\\Automation\\Driver\\chromedriver.exe"); // setup system environment variable for chrome driver
             driver = new ChromeDriver();
         }
         else if(browser.equals("firefox")){
-            System.setProperty("webdriver.gecko.driver", "D:\\Selenium\\driver\\geckodriver.exe"); // setup system environment variable for firefox driver
+            System.setProperty("webdriver.gecko.driver", currentDir + "\\Automation\\Driver\\geckodriver.exe"); // setup system environment variable for firefox driver
             driver = new FirefoxDriver();
         }
         else if(browser.equals("IE")){
-            System.setProperty("webdriver.ie.driver", "D:\\Selenium\\driver\\IEDriverServer.exe"); // setup system environment variable for firefox driver
+            System.setProperty("webdriver.ie.driver", currentDir + "\\Automation\\Driver\\IEDriverServer.exe"); // setup system environment variable for firefox driver
             driver = new InternetExplorerDriver();
         }
         else{
